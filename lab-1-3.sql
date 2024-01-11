70ff23b6be4b585b045a084bc88f0403c22ce98b
Ben Morton <ben.morton@kellogg.northwestern.edu>
2024-01-11 01:40:21 +0000

    test

--- lab-1-3.sql
+++ lab-1-3.sql
@@ -3,7 +3,7 @@
 SELECT year, name, wins, losses FROM teams ORDER BY losses DESC LIMIT 3; 
 
 -- Expected result:
---
+--dd
 -- +------+----------------------+------+--------+
 -- | 1899 | Cleveland Spiders    | 20   | 134    |
 -- | 1962 | New York Mets.       | 40   | 120    |
